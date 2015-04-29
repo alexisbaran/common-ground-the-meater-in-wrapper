1d522c164814e18550f6544ad8382897ae67e074
bydo <cdr@heiga.org>
2015-04-28 20:48:58 -0400

    start good/bad/okay

--- js/meat-events.js
+++ js/meat-events.js
@@ -1,6 +1,7 @@
 $(document).ready (function () {
+    $(['.good','.bad','.okay']).map(
     $('.good').click(function (g) {
         $('.good').map(function () { this.style.opacity = 1;});
-        // holy shit there has to be a better way to do this.
+    // holy shit there has to be a better way to do this.
     });
 });
