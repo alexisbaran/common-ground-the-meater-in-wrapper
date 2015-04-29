c6e553932e3b33a75807d56858ab213b702b80a1
bydo <cdr@heiga.org>
2015-04-29 06:58:38 -0400

    remove deadweight in js, make slider not reset to 0 when own meat is clicked

--- js/meat-events.js
+++ js/meat-events.js
@@ -2,15 +2,13 @@ $(document).ready (function () {
     $('.about-text.hidden').css({opacity : 1});
     $(['.good','.bad','.okay']).map(function (i,cl) {
         $(cl+'.hidden-meater').click(function () {
+            if ($(cl+'.hidden').css('opacity') == 0) {
+                eval(cl.slice(1) + '_slidr.slide("one")');
+            }
             $('.hidden').css({opacity : 0});
             $('.hidden-meater img').css({opacity: 1});
             $(cl+'.hidden').css({opacity : 1});
             $(cl+'.hidden-meater img').css({opacity: 0});
-            eval(cl.slice(1) + '_slidr.slide("one")');
         });
     });
-    
-    $('.themeater').click(function () {
-        $(this).css({opacity : 1});
-    });    
 });
