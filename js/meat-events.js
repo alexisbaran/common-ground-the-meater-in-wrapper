bdb26b6fd9e6e53b1d4819447b955079dada3951
bydo <cdr@heiga.org>
2015-04-29 19:28:05 -0400

    *** empty log message ***

--- js/meat-events.js
+++ js/meat-events.js
@@ -3,7 +3,10 @@ $(document).ready (function () {
     $(['.good','.bad','.okay']).map(function (i,cl) {
         $(cl+'.hidden-meater').click(function () {
             if ($(cl+'.hidden').css('opacity') == 0) {
-                eval(cl.slice(1) + '_slidr.slide("one")');
+                sl = eval(cl.slice(1) + '_slidr');
+                sl.timing('linear', '0.0s');
+                sl.slide("one");
+                sl.timing('linear', '0.5s ease-in');
             }
             $('.hidden').css({opacity : 0});
             $('.hidden-meater img').css({opacity: 1});
