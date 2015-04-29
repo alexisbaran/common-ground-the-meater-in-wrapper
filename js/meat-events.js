3fbe57f887f8460bad68dc14034e09001d15acc0
bydo <cdr@heiga.org>
2015-04-28 21:10:06 -0400

    clicking too fine

--- js/meat-events.js
+++ js/meat-events.js
@@ -1,7 +1,8 @@
 $(document).ready (function () {
-    $(['.good','.bad','.okay']).map(
-    $('.good').click(function (g) {
-        $('.good').map(function () { this.style.opacity = 1;});
-    // holy shit there has to be a better way to do this.
+    $(['.good','.bad','.okay']).map(function (i,cl) {
+        console.log(cl);
+        $(cl).click(function (g) {
+            $(this).map(function (i,e) { e.style.opacity = 1;})
+        });
     });
 });
