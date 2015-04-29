6bd7b5ebec0df441f54bc6bd25ed12149cea53f1
bydo <cdr@heiga.org>
2015-04-28 21:57:14 -0400

    hax

--- js/meat-events.js
+++ js/meat-events.js
@@ -1,6 +1,7 @@
 $(document).ready (function () {
     $(['.good','.bad','.okay']).map(function (i,cl) {
         $(cl).click(function () {
+            $('.hidden').css({opacity : 0});
             $(cl+'.hidden').css({opacity : 1});
         });
     });
